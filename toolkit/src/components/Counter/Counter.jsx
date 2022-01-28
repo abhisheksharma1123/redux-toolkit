bdc83afc397794dee104514b4896a314b5c258c9
Abhishek <56558684+abhisheksharma1123@users.noreply.github.com>
2022-01-28 02:16:57 +0000

    bug-fix

--- toolkit/src/components/Counter/Counter.jsx
+++ toolkit/src/components/Counter/Counter.jsx
@@ -3,7 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "./counterSlice";
 
 const Counter = () => {
-  const count = useSelector((state) => state.Counter.value);
+  // state has shape : {"counter":{"value":0}}
+  const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
   return (
     <div>
