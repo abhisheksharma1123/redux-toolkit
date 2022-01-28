820a2cb7d1179bc974a363e95cb58eb393b2dda7
Abhishek <56558684+abhisheksharma1123@users.noreply.github.com>
2022-01-28 01:27:37 +0000

    destructure-actions-and-reducer

--- toolkit/src/components/counter/counterSlice.js
+++ toolkit/src/components/counter/counterSlice.js
@@ -4,6 +4,7 @@ const initialState = {
   value: 0,
 };
 const options = {
+  // the below name property will automatically be taken by type prop in action
   name: "counter",
   initialState,
   reducers: {
@@ -24,3 +25,6 @@ const options = {
 };
 
 const counterSlice = createSlice(options);
+// createSlice method returns an object and contains several properties such as .actions, .reducer
+export const { increment, incrementByAmount, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
