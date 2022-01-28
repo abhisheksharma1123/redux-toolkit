ca0722de8e36d69263a42d1333cec2bb2b3b4c24
Abhishek <56558684+abhisheksharma1123@users.noreply.github.com>
2022-01-28 02:20:54 +0000

    add-comment

--- toolkit/src/components/Counter/Counter.jsx
+++ toolkit/src/components/Counter/Counter.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "./counterSlice";
 
 const Counter = () => {
-  // state has shape : {"counter":{"value":0}}
+  // state has shape : {"counter":{"value":0}} - // counter is the name of slice tha's why we need to access state.counter
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
   return (
